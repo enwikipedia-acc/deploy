535c3ab417d41116699b53167bc0e839e350bdda
Simon Walker <github@stwalkerster.co.uk>
2012-07-05 00:31:07 +0100

    deploy script extracted from API

---
+++ deploy.php
@@ -0,0 +1,23 @@
+<?php
+
+	require_once ("config.inc.php");
+	
+	$revision = isset( $_GET['r'] ) ? $_GET['r'] : '';
+	if( $revision == '' ) {
+		echo("Please specify a revision");
+		die;
+	}
+	
+	$key = isset( $_GET['k'] ) ? $_GET['k'] : '';
+	if( $key == '' ) {
+		echo("Please specify a key");
+		die;		
+	}
+	
+	if( md5( md5($revision) . $apiDeployPassword ) != $key ) {
+		echo("Invalid key.");
+		die;
+	}
+	
+	passthru( './deploy.sh ' . $revision  );
+	die;
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ../sand/
+
+git remote update origin
+
+git checkout $1
