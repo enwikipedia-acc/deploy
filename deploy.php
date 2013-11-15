2937bdcf01ae124467eca621e2fe8b3e63ef0b61
Simon Walker <github@stwalkerster.co.uk>
2013-11-15 03:05:04 +0000

    add ignore list

--- deploy.php
+++ deploy.php
@@ -2,6 +2,20 @@
 
 	require_once ("config.inc.php");
 	
+	$ignorelist = array(
+		"Fetching origin",
+		"Fetching RileyHuntley",
+		"Fetching stwalkerster",
+		"Fetching FunPika",
+		"Fetching JohnFLewis",
+		"Fetching cyberpower678",
+		"You are not currently on a branch, so I cannot use any",
+		"'branch.<branchname>.merge' in your configuration file.",
+		"Please specify which remote branch you want to use on the command",
+		"line and try again (e.g. 'git pull <repository> <refspec>').",
+		"See git-pull(1) for details",
+	);
+	
 	$revision = isset( $_GET['r'] ) ? $_GET['r'] : '';
 	if( $revision == '' ) {
 		echo("Please specify a revision");
@@ -24,7 +38,10 @@
 	
 	foreach( $output as $line )
 	{
-		echo $line . "\n";
+		if(! in_array( $line, $ignorelist ) )
+		{
+			echo $line . "\n";
+		}
 	}
 	
 	die;
