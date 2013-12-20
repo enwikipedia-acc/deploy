a32488461ee7f6b55becdecc5fdec82ff2003846
Simon Walker <simon@stwalkerster.co.uk>
2013-12-20 04:12:50 +0000

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -9,11 +9,18 @@
 		"Fetching FunPika",
 		"Fetching JohnFLewis",
 		"Fetching cyberpower678",
+		"Fetching t13",
 		"You are not currently on a branch, so I cannot use any",
 		"'branch.<branchname>.merge' in your configuration file.",
 		"Please specify which remote branch you want to use on the command",
 		"line and try again (e.g. 'git pull <repository> <refspec>').",
 		"See git-pull(1) for details.",
+		"You are in 'detached HEAD' state. You can look around, make experimental",
+		"changes and commit them, and you can discard any commits you make in this",
+		"state without impacting any branches by performing another checkout.",
+		"If you want to create a new branch to retain commits you create, you may",
+		"do so (now or later) by using -b with the checkout command again. Example:",
+		"  git checkout -b new_branch_name",
 	);
 	
 	$revision = isset( $_GET['r'] ) ? $_GET['r'] : '';
